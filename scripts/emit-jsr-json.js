69d00defdc715b51dd13aeb5a6fda7cbce129d07
Samual Norman <me@samual.uk>
2025-06-13 11:48:44 +0100

    factor out the converting of npm to jsr deps a little so it can be easier configured

--- scripts/emit-jsr-json.js
+++ scripts/emit-jsr-json.js
@@ -3,13 +3,18 @@ import { mkdirSync as makeDirectorySync, writeFileSync } from "fs"
 import packageJson from "../package.json" with { type: "json" }
 import { getExports } from "./lib/exports.js"
 
+/** @type {Record<string, string>} */ const ConvertToJsr = {
+	"@standard-schema/spec": "@standard-schema/spec",
+	"@standard-schema/utils": "@standard-schema/utils"
+}
+
 const { name, version, license, dependencies } = packageJson
 
 makeDirectorySync("dist", { recursive: true })
 
 const imports = Object.fromEntries(
 	Object.entries(dependencies)
-		.map(([ name, version ],) => [ name, `${name == `@samual/lib` ? `npm` : `jsr`}:${name}@${version}`, ])
+		.map(([ name, version ],) => [ name, `${name in ConvertToJsr ? `jsr:${ConvertToJsr[name]}` : `npm:${name}`}@${version}`, ])
 )
 
 writeFileSync(
